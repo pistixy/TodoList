adc693d4cf1cc8a6b6b44737b92766c83c61edf1
pistixy <71815251+pistixy@users.noreply.github.com>
2023-10-17 15:02:17 +0200

    Todo App reminder?

--- script.js
+++ script.js
@@ -1,40 +1,84 @@
-const inputBox =document.getElementById("input-box")
-const listContainer =document.getElementById("list-container")
+// Check if the browser supports notifications and request permission
+if ('Notification' in window) {
+    Notification.requestPermission();
+}
 
-function addTask(){
-    if(inputBox.value===''){
-        alert("You must write something");
+const inputBox = document.getElementById("input-box");
+const reminderInput = document.getElementById("reminder-input");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    const taskText = inputBox.value.trim();
+    const reminderTime = new Date(reminderInput.value);
+
+    if (taskText === '' || isNaN(reminderTime)) {
+        alert("You must enter a task and set a valid reminder time.");
+        return;
     }
-    else{
-        let li=document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span =document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    const reminderDisplay = document.createElement("span");
+    reminderDisplay.className = "reminder";
+    reminderDisplay.textContent = formatReminderTime(reminderTime);
+
+    // Create a close button
+    const span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+
+    li.appendChild(reminderDisplay);
+    li.appendChild(span);
+    listContainer.appendChild(li);
+
+    const now = new Date();
+    const timeUntilReminder = reminderTime - now;
+
+    if (timeUntilReminder <= 0) {
+        alert("The reminder time must be in the future.");
+        return;
     }
-    inputBox.value="";
+
+    if (Notification.permission === 'granted') {
+        // Schedule a notification for the reminder time
+        setTimeout(() => {
+            const notification = new Notification('Reminder', {
+                body: taskText
+            });
+        }, timeUntilReminder);
+    }
+
+    inputBox.value = "";
+    reminderInput.value = "";
     saveData();
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName ==="LI"){
+function formatReminderTime(reminderTime) {
+    const options = {month: '2-digit', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+    return reminderTime.toLocaleDateString('en-US', options);
+}
+
+
+// Add click event listener for marking tasks as checked and removing them
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         saveData();
-    }
-    else if(e.target.tagName ==="SPAN"){
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
     }
-
 }, false);
 
-function saveData(){
+// Function to save task data in local storage
+function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
 
-function showTask(){
-    listContainer.innerHTML=localStorage.getItem("data")
+// Function to show tasks retrieved from local storage
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
 }
 
+// Initialize the task list from local storage
 showTask();
